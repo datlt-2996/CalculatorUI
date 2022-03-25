ab45f76b7a267c2fef1e9c09464d6a0d97001fb6
datlt-2996 <101380790+datlt-2996@users.noreply.github.com>
2022-03-25 16:00:21 +0700

    Update ViewController.swift

--- UICalculator/ViewController.swift
+++ UICalculator/ViewController.swift
@@ -135,11 +135,9 @@ class ViewController: UIViewController {
     @IBAction func devideButton(_ sender: Any) {
         if(!resultLabel.isEmpty){
             nextMath()
-            workings = workings + ".000"
             addToWorkings(value: "/")
         }
         else{
-            workings = workings + ".000"
             addToWorkings(value: "/")
         }
 
